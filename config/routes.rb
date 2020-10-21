f2a2320bcc5f26e833890abdcda25b125b3aad72
Gilberto Santos <giba.dosantos@gmail.com>
2020-10-20 21:26:52 -0300

    attempt

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # verb "url", to: "controller#action"
+  get "ask", to: "pages#ask"
+  get "answer", to: "pages#answer"
 end
